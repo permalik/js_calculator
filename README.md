9556ea08b4933c470bf3246abae0da397746db91
permalik <permalik@protonmail.com>
2021-06-13 17:20:34 -0500

    Initialize Project

---
+++ README.md
@@ -0,0 +1,1 @@
+JS Calculator is a tool that allows users to do simple arithmetic calculations.
---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    require('postcss-preset-env')({
+      autoprefixer: {
+        grid: 'autoplace',
+
+        add: true,
+
+        env: '.browserlistrc',
+
+        remove: true,
+
+        flexbox: true,
+      },
+    }),
+  ],
+};
